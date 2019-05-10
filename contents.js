af518e04a27fa8c9d3a055636202d1ab65046419
AtsushiYamamoto <amare0130@gmail.com>
2019-05-10 22:07:54 +0900

    またコードの内容教えてください！ 2

--- contents.js
+++ contents.js
@@ -1,55 +1,51 @@
-const video = document.getElementsByTagName('video');
+const video = document.getElementsByTagName("video");
 
 //  get current page URL
 const currURL = window.location.toString();
-const dotInstallURL = 'https://dotinstall.com/lessons/';
+const dotInstallURL = "https://dotinstall.com/lessons/";
 
 // ドットインストールを開いているか判定してページ読み込み時の動作
 if (currURL.indexOf(dotInstallURL) === 0) {
-    // Auto play the video
-    if (video.length) {
-        video[0].muted = false;
-        const pipPromise = video[0].requestPictureInPicture();
-        if (pipPromise !== null) {
-            pipPromise.catch(() => { video[0].requestPictureInPicture(); })
-        }
-        // chrome.storage.sync.get(["pipbtn"], function(btnState) {
-        //     btnState.pipbtn && video[0].requestPictureInPicture();
-        // });
-        video[0].play();
-    };
+  // Auto play the video
+  if (video.length) {
+    video[0].muted = false;
+    const pipPromise = video[0].requestPictureInPicture();
+    if (pipPromise !== null) {
+      pipPromise.catch(() => {
+        video[0].requestPictureInPicture();
+      });
+    }
+    // chrome.storage.sync.get(["pipbtn"], function(btnState) {
+    //     btnState.pipbtn && video[0].requestPictureInPicture();
+    // });
+    video[0].play();
+  }
 }
 // videoがあるページで所定のイベントを定義
 if (video.length) {
-    chrome.runtime.onMessage.addListener(function(request) {
-        // run "picture In picture", when clicking the pip button.
-        // When pip button again, became end the "picture in picture."
-        if (request.pipbtn) {
-            video[0].requestPictureInPicture();
-        } else {
-            document.exitPictureInPicture();
-        }
-    });
+  chrome.runtime.onMessage.addListener(function(request) {
+    // run "picture In picture", when clicking the pip button.
+    // When pip button again, became end the "picture in picture."
+    if (request.pipbtn) {
+      video[0].requestPictureInPicture();
+    } else {
+      document.exitPictureInPicture();
+    }
+  });
 
-    video[0].addEventListener('ended', function() {
-        // chrome.strageの状態を取得
-        chrome.storage.sync.get(["cntbtn", "cnpbtn"], function(btnState) {
-            // もし、連続再生がTrueなら次のページをクリック
-            btnState.cnpbtn && document.querySelector('#lesson-complete-button').click();
-            btnState.cntbtn && document.querySelector('#go_to_next_video').click();
-        });
+  video[0].addEventListener("ended", function() {
+    // chrome.strageの状態を取得
+    chrome.storage.sync.get(["cntbtn", "cnpbtn"], function(btnState) {
+      // もし、連続再生がTrueなら次のページをクリック
+      btnState.cnpbtn &&
+        document.querySelector("#lesson-complete-button").click();
+      btnState.cntbtn && document.querySelector("#go_to_next_video").click();
     });
+  });
 }
 
-
-
-
-
-
-
-
 // window.onload は別にいらないのかもしれない。
-window.onload = function() {}
+window.onload = function() {};
 
 // chrome.runtime.onMessage.addListener(function(request) {
 //     const video = document.getElementsByTagName('video');
@@ -57,6 +53,7 @@ window.onload = function() {}
 //     console.log(request)
 // // });
 
+//またこの内容教えてね♪
 
 // let test = () => {
 //     console.log('aa');
@@ -83,18 +80,15 @@ window.onload = function() {}
 
 // // const playClickDiv = document.getElementById('playClickDiv');
 
-
-
-
 // /**
 //  * Unmuted the active video
-//  * @function unmuted 
+//  * @function unmuted
 //  */
 // const unmuted = () => videoReference.muted = false;
 
-// /** 
+// /**
 //  * Pley the active video
-//  * @function goPlay 
+//  * @function goPlay
 //  */
 // const goPlay = () => videoReference.play();
 
@@ -105,27 +99,19 @@ window.onload = function() {}
 // const goPause = () => videoReference.pause();
 
 // /**
-//  * 
+//  *
 //  * @function getNextVideoLink
-//  * @retruns VideoLink as String 
+//  * @retruns VideoLink as String
 //  */
 // const getNextVideoLink = () => {
 
 // }
-
-
-
-
-
 
 // window.onload = () => {
 //         unmuted();
 //         // Checking supported the "picture in picture"  in this browser
 //         !document.pictureInPictureEnabled || video.disablePictureInPicture ?
 //             (togglePipButton.hidden, alert('このブラウザではPIPがサポートされていません!')) : console.log();
-
-
-
 
 //         // the active video is plaied, when click the play button.
 //         togglePlaybutton.addEventListener('click', async function() {
@@ -143,9 +129,6 @@ window.onload = function() {}
 //         });
 
 //         togglePlaybutton.click();
-
-
-
 
 //         // Note that this can happen if user clicked the "Toggle Picture-in-Picture"
 //         // button but also if user clicked some browser context menu or if
@@ -172,14 +155,12 @@ window.onload = function() {}
 //             console.log(`> Window size changed to ${pipWindow.width}x${pipWindow.height}`);
 //         }
 
-
 //         video.addEventListener('play', () => {
 //             togglePlaybutton.innerHTML = '<i class="far fa-pause-circle"></i> Stop'
 //         });
 //         video.addEventListener('pause', () => {
 //             togglePlaybutton.innerHTML = '<i class="far fa-play-circle"></i> Play'
 //         });
-
 
 // video.addEventListener('ended', () => {
 //     video.src = "https://player.vimeo.com/external/270306379.hd.mp4?s=c07d6e9c154f8c190caeb596c574258f466d57af&profile_id=174&oauth2_token_id=56271471"
